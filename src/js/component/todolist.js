6bcb76b52b06ad846f1832e98cd443fd5b50f9e7
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-05-21 09:15:01 +0000

    21/05 morning ok.Search how to do a put with fetch

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -2,18 +2,40 @@ import React, { useEffect, useState } from "react";
 
 const Todo = () =>{
     const [listItems,setlistItems] = useState([]);
+    const [createTask,setcreateTask]=useState("");
+    //      [valor act, funcion que modifica el valor] = useState("") 
     useEffect(() =>{
         fetch("https://assets.breatheco.de/apis/fake/todos/user/rubengargu")
         .then(response =>{
-            
+            //Linea 9: permite devolver los datos de la API
             return response.json()
         }
     ).then(response=>{setlistItems(response)})
     },[])
+
+    const newTask =(event) =>{
+        setcreateTask(event.target.value)
+        //Esta funcion permite guardar lo que ha escrito el usuario //
+        
+    }
+    const create=() =>{
+        let allTasks = [...listItems,createTask ]
+        setlistItems(allTasks) //Esto permite setear todas las tareas
+        //La linea tiene todas las tareas
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/rubengargu")
+        
+
+    }
      
     return(
         <div id="container">
-<p>setlistItems.map(</p>
+            <input type="text" name="newTask" onChange={newTask}/>
+            <button onClick={create} className="btn btn-primary" >Nueva tarea</button>
+
+<ul>{listItems.map((task,index)=>{
+    return <li key={index}>{task.label}</li>
+    
+})}</ul>
 
         </div>
         
